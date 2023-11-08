c2e3ac7a7f21a33e7315de14245c9296b53c82da
POZHILANVD <pozhilan4321@gmail.com>
2023-11-08 21:30:33 +0530

    Success

--- pozhilan/san/models.py
+++ pozhilan/san/models.py
@@ -5,6 +5,7 @@ class foodball_player(models.Model):
     p_age=models.IntegerField()
     p_country=models.CharField(max_length=20)
     p_email=models.EmailField()
+    p_salary=models.IntegerField()
 
 class foodball_playerAdmin(admin.ModelAdmin):
-    list_display=('p_name','p_age','p_country','p_email')
+    list_display=('p_name','p_age','p_country','p_email','P_salary')
